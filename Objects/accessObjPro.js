e6de596d7a36d7c0d0b641b5d6ea5cc2abe1eede
Abdisa Bedada <abdisat.bedada@gmail.com>
2024-02-13 23:04:31 -0600

    integrating html with js

---
+++ Objects/accessObjPro.js
@@ -0,0 +1,36 @@
+// access to object properties via dot notation
+
+/* const teacher = {
+    firstName: 'Amanda',
+    lastName: 'Bob',
+    age: 44,
+    skills: ['JS', 'ReactJS', 'Python'],
+    employed: true,
+    city: 'Miami',
+}
+console.log(teacher.firstName)
+console.log(teacher['firstName'])
+console.log(teacher.age)
+console.log(teacher['age'])
+console.log(teacher.skills)
+console.log(teacher['skills'])
+
+let key = 'first'
+console.log(teacher[key + 'Name'])
+let key1 = 'last'
+console.log(teacher[key + 'Name'])
+ */
+
+
+//Questions 
+// Ask a user a day, and display the working hours for that day.
+
+const workingHours = {
+monday: '09:00 - 17:00',
+tuesday: '09:00 - 13:00',
+wednesday: '09:00 - 14:00',
+thursday: '03:00 - 9:00',
+friday: '09:00 - 10:00',
+saturday: '09:00 - 15:00',
+sunday: 'closed'
+}
