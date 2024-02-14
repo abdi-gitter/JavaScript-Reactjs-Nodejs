197a9a7436c253c245142d2e482912f10310f0ef
Abdisa Bedada <abdisat.bedada@gmail.com>
2024-02-13 20:58:53 -0600

    Objects example

---
+++ Objects/objects.js
@@ -0,0 +1,24 @@
+const student = {
+    firstName: 'Jane',
+    lastName: 'smith',
+    age: 34,
+    skills: ['JS', 'ReactJS', 'NodeJS'],
+}
+
+const teacher = {
+    firstName: 'Amanda',
+    lastName: 'Bob',
+    age: 44,
+    skills: ['JS', 'ReactJS', 'Python'],
+    employed: true,
+    city: 'Miami',
+}
+const student2 = new Object()
+student2.firstName = 'Abdisa',
+student2.lastName = 'Bedada',
+student2.age = 44;
+student2.employed = false;
+
+console.log(student)
+console.log(teacher)
+console.log(student2)
