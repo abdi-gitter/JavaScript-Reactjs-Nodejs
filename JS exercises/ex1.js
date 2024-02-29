12d7c0824bee6efd65625f501ec1a9edc0768215
abdi-gitter <152942085+abdi-gitter@users.noreply.github.com>
2024-02-28 23:09:07 -0600

    Add files via upload

---
+++ JS exercises/ex1.js
@@ -0,0 +1,12 @@
+let num = prompt("Enter a number:");
+num = parseInt(num); 
+
+let output;
+
+if (num > 20) {
+    output = (num - 20) * 2;
+} else {
+    output = 20 - num;
+}
+
+console.log(output);
