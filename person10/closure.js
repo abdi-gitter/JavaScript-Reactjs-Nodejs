cb83faad5115a9bb3c0bcfee021c056874868516
Abdisa Bedada <abdisat.bedada@gmail.com>
2024-03-04 00:04:02 -0600

    Simple closure

---
+++ person10/closure.js
@@ -0,0 +1,13 @@
+// Closure
+function createCounter(){
+    let count = 0
+    return function(){
+        count += 1
+        return count
+    };
+}
+let counter = createCounter()
+console.log(counter())
+console.log(counter())
+console.log(counter())
+console.log(counter())
