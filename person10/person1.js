92a61432326fb1f8162d826335e307c861759893
Abdisa Bedada <abdisat.bedada@gmail.com>
2024-03-03 20:28:24 -0600

    Added Tizita

--- person10/person1.js
+++ person10/person1.js
@@ -3,6 +3,9 @@ function Person(firstName, lastName){
     this.lastName = lastName
     }
     Person.prototype.nationality = 'American'
+    Person.prototype.getFullName = function() {
+       return this.firstName + ' ' + this.lastName
+    };
     const person1 = new Person('Abdisa', 'Bedada')
     console.log(person1.nationality)
 
@@ -13,7 +16,9 @@ function Person(firstName, lastName){
     console.log(person2.nationality)
   
     const person3 = new Person('Cage', 'Ragnar')
+    let person10 = new Person('Tizita', 'Tullu')
     person3.nationality = 'Chines'
     console.log(person1)
     console.log(person2)
     console.log(person3)
+    console.log(person10.getFullName())
