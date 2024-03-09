a48fbad90eecc8d25986b125bcfadcdad6c81063
abdi-gitter <152942085+abdi-gitter@users.noreply.github.com>
2024-03-08 20:36:48 -0600

    Add files via upload

---
+++ Hof-example/hof-eg.js
@@ -0,0 +1,34 @@
+const radius = [3, 2, 1, 4];
+const calculateArea = function (radius) {
+    const output = [];
+    for(let i=0; i < radius.length; i++){
+        output.push(Math.PI * radius[i] * radius[i]);
+    }
+    return output;
+  }
+  console.log(calculateArea(radius))
+
+
+
+
+  const calculateCircumference = function(radius){
+    const output = [];
+    for(let i=0; i < radius.length; i++){
+        output.push(2 * Math.PI * radius[i]);
+    }
+    return output;
+  }
+  console.log(calculateCircumference(radius))
+  
+
+
+
+  const calculateDiameter = function(radius){
+    const output = [];
+    for(let i=0; i < radius.length; i++){
+        output.push(2 * radius[i]);
+    }
+    return output;
+  }
+  console.log(calculateDiameter(radius))
+  
