6f436b8136cee4ff2db1678fd1c832c264b7b731
Abdisa T. Bedada <152942085+abdi-gitter@users.noreply.github.com>
2024-06-07 22:57:08 -0500

    Update app.js

--- async-example/app.js
+++ async-example/app.js
@@ -1,5 +1,5 @@
 // set up event listeners for buttons #search #clear
-// Search:
+// Search://
 document.getElementById('search').addEventListener('click', () => {
     const search = document.getElementById('input').value
     console.log(search)
