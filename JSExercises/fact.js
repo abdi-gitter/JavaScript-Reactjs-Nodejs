58177c5339aac375dbd419a189d868242b858b02
Abdisa T. Bedada <152942085+abdi-gitter@users.noreply.github.com>
2024-06-30 16:07:00 -0500

    Update fact.js

--- JSExercises/fact.js
+++ JSExercises/fact.js
@@ -1,4 +1,4 @@
-// write a function that calculates the factorial of a number:
+// write a function that calculates the factorial of a number
 // factorial(5) // >> 5! is 1 * 2 * 3 * 4 * 5, which is: 120
 function factorial(num){
     let fact = ""
