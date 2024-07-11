f7151054c9da9599e4b75cb787e100d0c61228ae
Abdisa T. Bedada <152942085+abdi-gitter@users.noreply.github.com>
2024-07-11 10:14:58 -0500

    Update this.js

--- thisKeyword/this.js
+++ thisKeyword/this.js
@@ -1,4 +1,4 @@
-//The this jeyword
+//The the this key word works
 /* Using the bind Method
 The value of this refers to the object it belongs to, 
 and it can change based on how a function is called.
