70ac74f6bbaab4cc6eeb60f9765e86cb8bf07771
Abdisa T. Bedada <152942085+abdi-gitter@users.noreply.github.com>
2024-07-24 10:44:16 -0500

    Update isLeapYear.js

--- promise2/isLeapYear.js
+++ promise2/isLeapYear.js
@@ -1,8 +1,9 @@
-//How to find a leap year// 
+//How to find a leap year in javascript// 
 
 //First, let's create a function isLeapYear that takes 
 //a year as an input and returns a Promise. The Promise 
 //resolves if the year is a leap year and rejects if it is not.
+
 function isLeapYear(year) {
     return new Promise((resolve, reject) => {
       if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
