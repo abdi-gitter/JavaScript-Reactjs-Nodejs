83a0d59ff7c856165ead501ec59d8732bc863b4f
Abdisa T. Bedada <152942085+abdi-gitter@users.noreply.github.com>
2024-07-31 19:36:47 -0500

    Update bind.js

--- oop/oop/bind.js
+++ oop/oop/bind.js
@@ -1,3 +1,7 @@
+// bind returns a new function with the context of the first argument
+// All this is doing is "binding" context to the function.
+// This means that the function will be called with the context of the object passed in as the first argument.
+
 function greet(name){
     console.log(`Hello, ${name}!  My favorite color is ${this.color}.`)
 }
@@ -6,9 +10,6 @@ const contextObj = {
     color: "blue"
 }
 
-// bind returns a new function with the context of the first argument
-// All this is doing is "binding" context to the function.
-// This means that the function will be called with the context of the object passed in as the first argument.
 const boundGreet = greet.bind(contextObj)
 
 boundGreet("Chase") // >> Hello, Chase!  My favorite color is blue.
