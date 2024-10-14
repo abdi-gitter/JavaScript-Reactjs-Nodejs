325b82971c3547851efc7242f6ab30898fa15307
Abdisa T. Bedada <152942085+abdi-gitter@users.noreply.github.com>
2024-10-14 16:58:39 -0500

    Update script.js

--- Async Assignments/auto-text-master/script.js
+++ Async Assignments/auto-text-master/script.js
@@ -3,6 +3,7 @@
 const textEl = document.getElementById('text');
 const speedEl = document.getElementById('speed');
 const text = 'We Love Programming in javascript! ♥';
+
 let idx = 1;
 let speed = 300 / speedEl.value;
 
