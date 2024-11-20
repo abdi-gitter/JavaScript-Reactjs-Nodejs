f3124d0a704167965b9e7cd559d286ae6c57c345
Abdisa T. Bedada <152942085+abdi-gitter@users.noreply.github.com>
2024-11-20 14:00:02 -0600

    Update app.js

--- Start-Coding/app.js
+++ Start-Coding/app.js
@@ -62,6 +62,8 @@ is equivalent to
 Circle.call({}, 1) //here {} specifies the target of this
 Circle.apply({}, [1,2,3]); //functions are objects
 
+
+
 /* Adding/Removing properties
 Objects in JS are dynamic, after we create them we can add extra
 property or delete property.
