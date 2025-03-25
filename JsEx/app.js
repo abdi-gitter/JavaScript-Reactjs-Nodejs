46bc7e91a9ff500b2051941e013d40d0e91c38ed
Abdisa T. Bedada <152942085+abdi-gitter@users.noreply.github.com>
2025-03-24 19:26:03 -0500

    Update app.js

--- JsEx/app.js
+++ JsEx/app.js
@@ -5,6 +5,7 @@
 
 
 
+
 //--------------------------------------------//
 //?  Question 1 Ask the user to input a number. Check the input of the user, 
 // if it is not a valid number then keep getting input from the user until provides
