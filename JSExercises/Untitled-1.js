1aa46bf21d62a22245e5b479502f114f50b978c4
Abdisa T. Bedada <152942085+abdi-gitter@users.noreply.github.com>
2025-06-10 19:58:03 -0500

    Update Untitled-1.js

--- JSExercises/Untitled-1.js
+++ JSExercises/Untitled-1.js
@@ -1,6 +1,5 @@
 //temprature converter
 //Celsius to Fahrenheit
-
 const tempInCelsius = 25;
 const tempInFahrenheit = (tempInCelsius * 9/5) + 32; //cel to fahr
 console.log(`${tempInCelsius}°C is equal to ${tempInFahrenheit }°F`);
