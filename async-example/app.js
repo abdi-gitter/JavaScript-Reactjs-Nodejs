cb1f093eaa88efb7ff1758fed907798fb1aa2787
Abdisa T. Bedada <152942085+abdi-gitter@users.noreply.github.com>
2025-06-12 23:48:29 -0500

    Update app.js

--- async-example/app.js
+++ async-example/app.js
@@ -8,7 +8,6 @@ document.getElementById('search').addEventListener('click', () => {
     searchCountry(search)
 })
 
-
 // Clear the countries container first:
 
 document.getElementById('clear').addEventListener('click', () => {
