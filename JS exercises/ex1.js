dd71cc37ee4ebc8a47e7d98fba05650779fa361e
Abdisa T. Bedada <152942085+abdi-gitter@users.noreply.github.com>
2025-08-11 19:15:06 -0500

    Update ex1.js

--- JS exercises/ex1.js
+++ JS exercises/ex1.js
@@ -1,6 +1,5 @@
 //prompt  response
 
-
 let num = prompt("Enter a number to display:");
 num = parseInt(num); 
 let output1;
@@ -12,3 +11,4 @@ if (num > 20) {
     output1 = 20 - num;
 }
 console.log(output1);
+
